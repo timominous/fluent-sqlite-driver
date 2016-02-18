3b9a5e349d61cd8bff299aa960504c5fd2e5aac2
timominous <timominous@gmail.com>
2016-02-18 23:23:41 +0800

    Changed csqlite url

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SQLiteDriver",
     dependencies: [ 
-   		.Package(url: "https://github.com/loganwright/csqlite.git", majorVersion: 0),
+   		.Package(url: "https://github.com/timominous/csqlite.git", majorVersion: 0),
         //.Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0) //testing
         .Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
     ]
