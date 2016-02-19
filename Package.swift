603bd85cec9c81a40310893c9a847ba888c64f3a
timominous <timominous@gmail.com>
2016-02-19 10:37:11 +0800

    Updated fluent package url

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [ 
    		.Package(url: "https://github.com/timominous/csqlite.git", majorVersion: 0),
         //.Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0) //testing
-        .Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
+        .Package(url: "https://github.com/timominous/fluent.git", majorVersion: 0)
     ]
 )
